04a8f0ab684e5e104559a335d11716239946af85
Ganapathi <gana301104@gmail.com>
2025-07-06 21:13:33 +0530

    actions

--- backend/src/main/java/com/example/graphqlex/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/example/graphqlex/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,7 +23,9 @@ public class SecurityConfiguration {
     private final JwtRequestFilter jwtRequestFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf->csrf.disable()).
+        http.
+                cors(Customizer.withDefaults()).
+                csrf(csrf->csrf.disable()).
                 authorizeHttpRequests(auth-> auth
                         .requestMatchers(
                                 "/graphql",
